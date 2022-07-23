1789d5e1f70b6adbae5b4f9c1f4f7119b6edc7c0
Akif Islam <iamakifislam@gmail.com>
2022-07-23 22:28:22 +0600

    implement chat feature of client-server

--- Client.java
+++ Client.java
@@ -8,32 +8,28 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
+        String HOST = "127.0.0.1";
+        int PORT = 6000;
+        System.out.println("Client Started");
+        Socket socket = new Socket(HOST, PORT);
+        System.out.println("Client Connected with Server");
+        Scanner sc = new Scanner(System.in);
 
-
+        System.out.println("Enter a message to send to server");
 
         while (true) {
-            String HOST = "127.0.0.1";
-            int PORT = 6000;
-            System.out.println("Client Started");
-            Socket socket = new Socket(HOST, PORT);
 
-            System.out.println("Client Connected with Server");
-            Scanner sc = new Scanner(System.in);
-
-            System.out.println("Enter a message to send to server");
             String message = sc.nextLine();
 
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream()); // To Send data to Server
             ObjectInputStream ois  = new ObjectInputStream(socket.getInputStream()); // To Receive Data from Server
 
 
-            System.out.println("Thanks");
-
             oos.writeObject(message); // Sending to Server
 
             try {
                 Object fromServerReply = ois.readObject();
-                System.out.println("From Server : " + (String) fromServerReply);
+                System.out.println("Server : " + (String) fromServerReply);
 
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
--- Server.java
+++ Server.java
@@ -1,3 +1,8 @@
+// This Client-Server can chat together but only 1 message at each time.
+// Suppose, Client can only send 1 message to Server and cannot send anymore until he/she gets reply from Server.
+// This is a problem and I hope I will solve in next update.
+// Akif Islam
+
 package io.github.akifislam;
 
 import java.io.IOException;
@@ -5,26 +10,28 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = new ServerSocket(6000); // Jodi Port Already Busy thake
         System.out.println("Server Started on PORT 6000.\nListening ....");
+        Socket socket = serverSocket.accept(); // Kew Connect hote chaile Accept korbe
+        System.out.println("Server has accepted a new connection");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
 
-        while(true) {
-            Socket socket = serverSocket.accept(); // Kew Connect hote chaile Accept korbe
-            System.out.println("Server has accepted a new connection");
             ObjectInputStream ois = new ObjectInputStream(socket.getInputStream()); // To Receive Data from Client
             ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream()); // To Send data to Client
 
             try {
                 Object clientMessage = ois.readObject();
-                System.out.println("Received : " +clientMessage);
-                System.out.println("From Client : " + (String)clientMessage);
+                System.out.println("Client : " + (String) clientMessage);
 
                 String serverMessage = (String) clientMessage;
-                serverMessage = serverMessage.toUpperCase();
+                String toClientMessage = sc.nextLine();
+                serverMessage = toClientMessage;
 
                 // Sent to Client
                 oos.writeObject(serverMessage);
@@ -33,7 +40,6 @@ public class Server {
                 e.printStackTrace();
                 System.out.println("Could not receive message from client :( ");
             }
-            System.out.println("End");
 
         }
 
